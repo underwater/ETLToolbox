419ca23437cda76dacc68434ab2dd7c73460dd14
Adriana Andronic <adriana.andronic@gmail.com>
2019-08-08 13:08:41 +0200

    Added fake loan system WCF server

---
+++ LoanSystem.Api/Program.cs
@@ -0,0 +1,21 @@
+﻿using LoanSystem.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanSystem.Api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var host = new ServiceHost(typeof(Loans)))
+                host.Open();
+            Console.WriteLine("Host started @ : " + DateTime.Now);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LoanSystem.Core/Loans.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanSystem.Core
+{
+    public class Loans : ILoans
+    {
+        public void CreateLoan(string loan)
+        {
+            Console.WriteLine($"added a new loan {loan}");
+        }
+    }
+}
